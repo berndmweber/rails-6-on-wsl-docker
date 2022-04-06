6da49250df411c0c9c9bddc24958800bb31eff63
Bernd Weber <1895847+berndmweber@users.noreply.github.com>
2022-04-06 15:27:38 -0700

    Making generic

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ set -e
 bundle check || bundle install --jobs 20 --retry 5
 
 # Remove a potentially pre-existing server.pid for Rails.
-rm -f /myapp/tmp/pids/server.pid
+rm -f /rails_app/tmp/pids/server.pid
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
